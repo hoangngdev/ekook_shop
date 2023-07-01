1452d8c08ee9682d8c9770c9389766e2bb180950
DESKTOP-QD8NLVR\Hoang <nguyen.tud@gmail.com>
2023-07-01 09:50:43 +0200

    Implement login page

--- ebook_shop_frontend/src/pages/login/login.jsx
+++ ebook_shop_frontend/src/pages/login/login.jsx
@@ -1,8 +1,76 @@
+import React, { useState } from 'react';
+import { Button, Checkbox, Divider, Form, Input, message, notification } from 'antd';
+//import './register.scss';
+import { useNavigate } from 'react-router-dom';
+import { callLogin } from '../../services/api'
 const LoginPage = () => {
+
+    const navigate = useNavigate();
+
+    const onFinish = async (values) => {
+        const { email, password } = values;
+        const res = await callLogin(email, password);
+        if (res?.data?.access_token) {
+            message.success('Login successful!');
+            navigate('/')
+        } else {
+            notification.error({
+                message: "Login failed",
+                description:
+                    res.message && Array.isArray(res.message) ? res.message[0] : res.message,
+                duration: 5
+            })
+        }
+    };
+
     return (
-        <>
-            Login page
-        </>
+        <div className="login-page">
+            <main className="main">
+                <div className="container">
+                    <section className="wrapper">
+                        <div className="heading">
+                            <h2 className="text text-large">User login</h2>
+                            <Divider />
+                        </div>
+                        <Form
+                            name="basic"
+                            labelCol={{ span: 8 }}
+                            wrapperCol={{ span: 16 }}
+                            style={{ maxWidth: 600 }}
+                            initialValues={{ remember: true }}
+                            onFinish={onFinish}
+                            autoComplete="off"
+                        >
+                            <Form.Item
+                                label="Email:"
+                                name="email"
+                                rules={[{ required: true, message: 'Please input your email!' }]}
+                            >
+                                <Input />
+                            </Form.Item>
+
+                            <Form.Item
+                                label="Password:"
+                                name="password"
+                                rules={[{ required: true, message: 'Please input your password!' }]}
+                            >
+                                <Input.Password />
+                            </Form.Item>
+
+                            <Form.Item name="remember" valuePropName="checked" wrapperCol={{ offset: 8, span: 16 }}>
+                                <Checkbox>Remember me</Checkbox>
+                            </Form.Item>
+
+                            <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+                                <Button type="primary" htmlType="submit">
+                                    Submit
+                                </Button>
+                            </Form.Item>
+                        </Form>
+                    </section>
+                </div>
+            </main>
+        </div>
     )
 }
 
--- ebook_shop_frontend/src/pages/register/register.jsx
+++ ebook_shop_frontend/src/pages/register/register.jsx
@@ -75,10 +75,6 @@ const RegisterPage = () => {
                                 <Input />
                             </Form.Item>
 
-                            <Form.Item name="remember" valuePropName="checked" wrapperCol={{ offset: 8, span: 16 }}>
-                                <Checkbox>Remember me</Checkbox>
-                            </Form.Item>
-
                             <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
                                 <Button type="primary" htmlType="submit">
                                     Submit
